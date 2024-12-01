38fbd2cb2c2241071034f812fcedd7949f65b8c0
QuetschekucheXXL <172083870+QuetschekucheXXL@users.noreply.github.com>
2024-12-01 19:50:36 +0100

    Solution to part 2

--- day01/solution1.py
+++ day01/solution1.py
@@ -3,6 +3,7 @@ file_path = "task1.txt"
 left_side = []
 right_side = []
 
+# PART 1
 with open (file_path, "r") as file:
     #reading the file and storing the values in two lists. The left side in one an the right side in one:
     for line in file:
@@ -23,3 +24,17 @@ with open (file_path, "r") as file:
         else:
             total += toSum
     print(total)
+
+
+# PART 2
+
+    # start a new dictionary ...
+    to_multipliy = {}
+    # with the values of left_side as keys and the number of times they occur in right_side as values
+    for item in left_side:
+        to_multipliy[item] = right_side.count(item)
+    # summing up each entry in the dictionary by multiplying the value of the key with the value itself
+    result = sum(key * value for key, value in to_multipliy.items())
+
+
+    print(result)
